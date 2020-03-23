ab86950fb0c94c87e8e0735b77cf95b2214bee3f
Svein Are Danielsen <svein.danielsen@inmeta.no>
2020-03-23 11:03:49 +0100

    added links on the sidebar to the admin layout

--- src/app/core/layout/admin-sidebar/admin-sidebar.component.ts
+++ src/app/core/layout/admin-sidebar/admin-sidebar.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-admin-sidebar',
   templateUrl: './admin-sidebar.component.html',
-  styleUrls: ['./admin-sidebar.component.scss']
+  styleUrls: ['./admin-sidebar.component.scss'],
 })
 export class AdminSidebarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
