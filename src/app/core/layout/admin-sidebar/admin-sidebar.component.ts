b7c6013e42ba371d8ebda10ff40b1e7f3b863611
Svein Are Danielsen <svein.danielsen@inmeta.no>
2020-03-23 13:37:32 +0100

    implemented menu service to admin sidebar

--- src/app/core/layout/admin-sidebar/admin-sidebar.component.ts
+++ src/app/core/layout/admin-sidebar/admin-sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuService } from '@core/services/menu.service';
+import { Menu } from '@core/models/menu.model';
 
 @Component({
   selector: 'app-admin-sidebar',
@@ -6,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-sidebar.component.scss'],
 })
 export class AdminSidebarComponent implements OnInit {
-  constructor() {}
+  private readonly menuSlug = 'admin-menu';
+  menu: Menu;
 
-  ngOnInit(): void {}
+  constructor(private menuService: MenuService) {}
+
+  ngOnInit(): void {
+    this.menuService.getMenuBySlug(this.menuSlug).subscribe(result => {
+      if (result.length === 1) {
+        this.menu = result[0];
+      }
+    });
+  }
 }
