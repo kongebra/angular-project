d61345bd89834d93490f178b1d7ce5e57d70f38f
Svein Are Danielsen <svein.danielsen@inmeta.no>
2020-03-23 11:37:33 +0100

    improved blog header

--- src/app/core/layout/blog-header/blog-header.component.ts
+++ src/app/core/layout/blog-header/blog-header.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-blog-header',
   templateUrl: './blog-header.component.html',
-  styleUrls: ['./blog-header.component.scss']
+  styleUrls: ['./blog-header.component.scss'],
 })
 export class BlogHeaderComponent implements OnInit {
+  @Input() color = 'primary';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
