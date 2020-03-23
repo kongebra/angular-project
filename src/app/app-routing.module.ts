a9e3d870455499ecb2286aeae0ea73b0930e73f7
Svein Are Danielsen <svein.danielsen@inmeta.no>
2020-03-23 09:43:57 +0100

    admin feature added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [{ path: 'blog', loadChildren: () => import('./features/blog/blog.module').then(m => m.BlogModule) }];
+const routes: Routes = [{ path: 'blog', loadChildren: () => import('./features/blog/blog.module').then(m => m.BlogModule) }, { path: 'admin', loadChildren: () => import('./features/admin/admin.module').then(m => m.AdminModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
