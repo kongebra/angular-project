00b5308a948a48ef78eea44803b5361379872b31
Svein Are Danielsen <svein.danielsen@inmeta.no>
2020-03-23 13:37:07 +0100

    added http client module cleanup

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,6 +4,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { HttpClientModule } from '@angular/common/http';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 import { BlogLayoutComponent } from './layout/blog-layout/blog-layout.component';
 import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
 import { BlogHeaderComponent } from './layout/blog-header/blog-header.component';
@@ -11,12 +20,7 @@ import { BlogFooterComponent } from './layout/blog-footer/blog-footer.component'
 import { AdminHeaderComponent } from './layout/admin-header/admin-header.component';
 import { AdminSidebarComponent } from './layout/admin-sidebar/admin-sidebar.component';
 
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatListModule } from '@angular/material/list';
-import { MatIconModule } from '@angular/material/icon';
-import { MatDividerModule } from '@angular/material/divider';
+import { MenuService } from './services/menu.service';
 
 @NgModule({
   declarations: [
@@ -33,6 +37,7 @@ import { MatDividerModule } from '@angular/material/divider';
     BrowserAnimationsModule,
     RouterModule,
     FlexLayoutModule,
+    HttpClientModule,
 
     // material
     MatSidenavModule,
@@ -41,7 +46,9 @@ import { MatDividerModule } from '@angular/material/divider';
     MatListModule,
     MatIconModule,
     MatDividerModule,
+    MatProgressSpinnerModule,
   ],
+  providers: [MenuService],
   exports: [
     // layouts
     BlogLayoutComponent,
