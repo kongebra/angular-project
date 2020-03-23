8ffc1aebecb761a6a95fa2ad97bf89fa85aae57c
Svein Are Danielsen <svein.danielsen@inmeta.no>
2020-03-23 13:40:13 +0100

    added menus page for admin feature

--- src/app/features/admin/admin.module.ts
+++ src/app/features/admin/admin.module.ts
@@ -6,9 +6,10 @@ import { AdminRoutingModule } from './admin-routing.module';
 
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { MenusComponent } from './pages/menus/menus.component';
 
 @NgModule({
-  declarations: [HomeComponent, NotFoundComponent],
+  declarations: [HomeComponent, NotFoundComponent, MenusComponent],
   imports: [SharedModule, AdminRoutingModule],
 })
 export class AdminModule {}
