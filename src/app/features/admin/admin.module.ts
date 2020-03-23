7aaa4505034299e9284bed355e1246ece43317c8
Svein Are Danielsen <svein.danielsen@inmeta.no>
2020-03-23 11:07:44 +0100

    auto import to admin module

--- src/app/features/admin/admin.module.ts
+++ src/app/features/admin/admin.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '@shared/shared.module';
 
 import { AdminRoutingModule } from './admin-routing.module';
-import { AdminComponent } from './admin.component';
+
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
-  declarations: [AdminComponent],
+  declarations: [HomeComponent, NotFoundComponent],
   imports: [SharedModule, AdminRoutingModule],
 })
 export class AdminModule {}
