9dc46032be8f5feb570c494434724549b2b67224
Svein Are Danielsen <svein.danielsen@inmeta.no>
2020-03-23 10:22:49 +0100

    added pages to blog module

--- src/app/features/blog/blog-routing.module.ts
+++ src/app/features/blog/blog-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { BlogComponent } from './blog.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-const routes: Routes = [{ path: '', component: BlogComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class BlogRoutingModule { }
+export class BlogRoutingModule {}
--- src/app/features/blog/blog.module.ts
+++ src/app/features/blog/blog.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '@shared/shared.module';
 
 import { BlogRoutingModule } from './blog-routing.module';
-import { BlogComponent } from './blog.component';
+
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
-  declarations: [BlogComponent],
+  declarations: [HomeComponent, AboutComponent, ContactComponent],
   imports: [SharedModule, BlogRoutingModule],
 })
 export class BlogModule {}
