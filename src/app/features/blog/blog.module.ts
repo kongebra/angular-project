105321d996cc8da729973866e466ef7917638c44
Svein Are Danielsen <svein.danielsen@inmeta.no>
2020-03-23 11:13:44 +0100

    added not found page to blog

--- src/app/features/blog/blog.module.ts
+++ src/app/features/blog/blog.module.ts
@@ -7,9 +7,10 @@ import { BlogRoutingModule } from './blog-routing.module';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
-  declarations: [HomeComponent, AboutComponent, ContactComponent],
+  declarations: [HomeComponent, AboutComponent, ContactComponent, NotFoundComponent],
   imports: [SharedModule, BlogRoutingModule],
 })
 export class BlogModule {}
