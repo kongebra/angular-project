44580e9fcff75787185fcf0cc553e83ac94469f7
Svein Are Danielsen <svein.danielsen@inmeta.no>
2020-03-23 11:38:11 +0100

    fixed blog routing

--- src/app/features/blog/blog-routing.module.ts
+++ src/app/features/blog/blog-routing.module.ts
@@ -4,11 +4,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+
+  { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
+
+  { path: '404', component: NotFoundComponent },
+  { path: '**', redirectTo: '404' },
 ];
 
 @NgModule({
